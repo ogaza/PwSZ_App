2864407b41db395b3ca769d24c6d8d5a9ecf6a75
ogaza <ogaza@future-processing.com>
2016-10-12 16:41:56 +0200

    add hibernate reference

---
+++ mojbudzet.sql
@@ -0,0 +1,106 @@
+-- phpMyAdmin SQL Dump
+-- version 4.5.1
+-- http://www.phpmyadmin.net
+--
+-- Host: 127.0.0.1
+-- Czas generowania: 11 Paź 2016, 17:33
+-- Wersja serwera: 10.1.16-MariaDB
+-- Wersja PHP: 5.5.38
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Baza danych: `mojbudzet`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Struktura tabeli dla tabeli `kategoria`
+--
+
+CREATE TABLE `kategoria` (
+  `id` int(11) NOT NULL,
+  `nazwa` varchar(100) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Zrzut danych tabeli `kategoria`
+--
+
+INSERT INTO `kategoria` (`id`, `nazwa`) VALUES
+(1, 'kategoria 1'),
+(3, '222');
+
+-- --------------------------------------------------------
+
+--
+-- Struktura tabeli dla tabeli `wpis`
+--
+
+CREATE TABLE `wpis` (
+  `id` int(11) NOT NULL,
+  `typ` tinyint(4) NOT NULL,
+  `kategoriaId` int(11) NOT NULL,
+  `wartosc` decimal(10,0) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Zrzut danych tabeli `wpis`
+--
+
+INSERT INTO `wpis` (`id`, `typ`, `kategoriaId`, `wartosc`) VALUES
+(1, 1, 1, '10'),
+(2, -1, 1, '5');
+
+--
+-- Indeksy dla zrzutów tabel
+--
+
+--
+-- Indexes for table `kategoria`
+--
+ALTER TABLE `kategoria`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `wpis`
+--
+ALTER TABLE `wpis`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `kategoriaId` (`kategoriaId`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT dla tabeli `kategoria`
+--
+ALTER TABLE `kategoria`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+--
+-- AUTO_INCREMENT dla tabeli `wpis`
+--
+ALTER TABLE `wpis`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+--
+-- Ograniczenia dla zrzutów tabel
+--
+
+--
+-- Ograniczenia dla tabeli `wpis`
+--
+ALTER TABLE `wpis`
+  ADD CONSTRAINT `KategoriaId` FOREIGN KEY (`kategoriaId`) REFERENCES `kategoria` (`id`) ON DELETE CASCADE ON UPDATE CASCADE;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
